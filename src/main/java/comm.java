986fd9205b80765b539b2b8cc58a76f58d1e12ea
Juyang <929746999@qq.com>
2021-05-31 21:42:24 +0800

    修正

---
+++ src/main/java/comm.java
@@ -0,0 +1,14 @@
+/**
+ * comment的接口
+ * @author juyang
+ */
+interface comm {
+    int apply = 0;
+    String str = null;
+
+    /**
+     * 监听方法
+     */
+    void listen();
+
+}
--- src/main/java/newTestJ.java
+++ src/main/java/newTestJ.java
@@ -1,5 +1,12 @@
-public class newTestJ {
+
+public class newTestJ implements comm{
     public static void main(String[] args) {
         System.out.println("git生成");
+    }
+
+
+    @Override
+    public void listen() {
+
     }
 }
